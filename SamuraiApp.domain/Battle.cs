dfc78c79e2e20b04850797c55ef8155e4e1770d2
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-18 21:15:19 -0700

    update files

--- SamuraiApp.domain/Battle.cs
+++ SamuraiApp.domain/Battle.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
+namespace SamuraiApp.Domain
+{
+    
 
 public class Battle
 {
   public int BattleId { get; set; }   
   public string Name { get; set; }
   public List<Samurai>Samurais { get; set; } = new List<Samurai>();
+}
 }
